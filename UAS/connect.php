ea1b69d5d677e27206b97edae24f87d9ac3c480e
utaridp <96728490+utaridp@users.noreply.github.com>
2022-07-04 17:10:28 +0800

    Add files via upload

---
+++ UAS/connect.php
@@ -0,0 +1,10 @@
+<?php
+$servername='localhost';
+$username='root';
+$password='';
+$dbname='uas';
+
+$conn= new mysqli ("$servername","$username", "$password", "$dbname");
+//mysql_select_db($database);
+
+?>
